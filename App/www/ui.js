8f8ef9490857466a2503034c16380f097a663c3c
Margaux Haering <47325579+margauxhaering@users.noreply.github.com>
2025-04-11 14:18:45 +0200

    Add files via upload

--- App/www/ui.js
+++ App/www/ui.js
@@ -1,1 +1,17 @@
-
+// Wait for page load
+window.addEventListener("load", function () {
+  // Set normal title without HTML
+  var title = document.getElementsByTagName("title")[0];
+  title.innerText = "RNFuzzyApp : a RNA-seq Analysis App";
+  // Set favicon
+  var head = document.getElementsByTagName("head")[0];
+  var favicon = document.createElement("link");
+  favicon.rel = "shortcut icon";
+  favicon.type = "image/svg+xml";
+  favicon.href = "react.svg";
+  head.appendChild(favicon);
+  // Set good size for file button
+  var btn = document.querySelector("span.btn.btn-default.btn-file");
+  btn.style.padding = "9px 16px";
+  btn.style.marginBottom = "4px";
+});
